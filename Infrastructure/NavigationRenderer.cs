1fbd658a98a18dfbd4543a628aeb48b4e2875532
nipkin <daniel@meridium.se>
2026-03-20 21:53:04 +0100

    Fix nav

--- Infrastructure/NavigationRenderer.cs
+++ Infrastructure/NavigationRenderer.cs
@@ -10,18 +10,17 @@ namespace EpiPageImporter.Infrastructure
         private readonly UrlResolver _urlResolver = urlResolver;
         private readonly IPageRouteHelper _pageRouteHelper = pageRouteHelper;
 
-        public IHtmlContent RenderContentTree(ContentReference root, int maxDepth = 3)
+        public IHtmlContent RenderContentTree(ContentReference root)
         {
             var currentPage = _pageRouteHelper.Page;
 
             var sb = new StringBuilder();
-            Build(root, currentPage, 0, maxDepth, sb);
+            Build(root, currentPage, sb);
             return new HtmlString(sb.ToString());
         }
 
-        private void Build(ContentReference parent, PageData? currentPage, int depth, int maxDepth, StringBuilder sb)
+        private void Build(ContentReference parent, PageData? currentPage, StringBuilder sb)
         {
-            if (depth >= maxDepth) return;
 
             var children = _contentRepo.GetChildren<PageData>(parent);
             bool any = false;
@@ -49,8 +48,6 @@ namespace EpiPageImporter.Infrastructure
                 sb.AppendFormat("<a href=\"{0}\">{1}</a>",
                     url,
                     System.Net.WebUtility.HtmlEncode(child.Name));
-
-                Build(child.ContentLink, currentPage, depth + 1, maxDepth, sb);
 
                 sb.Append("</li>");
             }
